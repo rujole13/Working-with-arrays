1b37cbaa1289e572c8ac8bc44e7f6e00c5b490d4
Russell Lee <sjkrlee50@gmail.com>
2023-06-18 22:47:38 -0400

    filling arrays

--- script.js
+++ script.js
@@ -416,7 +416,7 @@ const accountMovements2 = accounts
   .flatMap(acc => acc.movements)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(accountMovements);
-*/
+
 
 // Strings
 const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
@@ -438,3 +438,24 @@ console.log(movements);
 
 movements.sort((a, b) => b - a);
 console.log(movements);
+*/
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+const x = new Array(7);
+console.log(x);
+
+// x.fill(1);
+x.fill(1, 3, 5);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+// Array.from()
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
